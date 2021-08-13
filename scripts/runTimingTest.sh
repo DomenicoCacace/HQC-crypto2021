c33dfff1a820067ad7ac398f1deb89918b45ece6
Domenico Cacace <domenico.cacace@mail.polimi.it>
2021-08-13 11:13:47 +0200

    Fix testing scripts
    Test only 128-bit version due to large memory footprint of 192 and 256 bit versions

--- scripts/runTimingTest.sh
+++ scripts/runTimingTest.sh
@@ -4,10 +4,10 @@ cmake -S .. -B ../build -DSECLVL=128 -DMODE="TIMING" -DCROSSCOMPILE=1 -DVERBOSE=
 make -C ../build flash
 ./serialRead.sh > temp
 
-cmake -S .. -B ../build -DSECLVL=192 -DMODE="TIMING" -DCROSSCOMPILE=1 -DVERBOSE=1
-make -C ../build flash
-./serialRead.sh
+#cmake -S .. -B ../build -DSECLVL=192 -DMODE="TIMING" -DCROSSCOMPILE=1 -DVERBOSE=1
+#make -C ../build flash
+#./serialRead.sh
 
-cmake -S .. -B ../build -DSECLVL=256 -DMODE="TIMING" -DCROSSCOMPILE=1 -DVERBOSE=1
-make -C ../build flash
-./serialRead.sh
+#cmake -S .. -B ../build -DSECLVL=256 -DMODE="TIMING" -DCROSSCOMPILE=1 -DVERBOSE=1
+#make -C ../build flash
+#./serialRead.sh
